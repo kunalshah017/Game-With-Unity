5d67f76bb286f46214b08595bb187d5a092bd87a
Kunal Shah <kunalmanishshah@gmail.com>
2022-11-30 17:41:38 +0530

    V1

---
+++ Basic Car Simulator/Assets/Scripts/CamFollowPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0.14f , 5.6f , -12.9f);
+    }
+}
