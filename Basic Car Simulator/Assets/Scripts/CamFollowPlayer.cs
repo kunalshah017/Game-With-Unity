268921558caa6567fdbd40c064090912a7dcea12
Kunal Shah <kunalmanishshah@gmail.com>
2022-12-04 16:21:02 +0530

    Vehicle Movement Added

--- Basic Car Simulator/Assets/Scripts/CamFollowPlayer.cs
+++ Basic Car Simulator/Assets/Scripts/CamFollowPlayer.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class CamFollowPlayer : MonoBehaviour
 {
+
     public GameObject player;
+    private Vector3 offset = new Vector3(0.14f, 5.6f, -12.9f);
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0.14f , 5.6f , -12.9f);
+        transform.position = player.transform.position + offset;
     }
 }
